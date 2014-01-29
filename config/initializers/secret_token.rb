97e951b768c7e98195297766542b262141e81f35
shobhavenk <shobha.venkatesh@strata.co.in>
2014-01-29 21:13:32 +0530

    Initialise repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorial::Application.config.secret_token = '1a81704fc74a89c9dc9b99032dbb88f5af37ac41506d3ba923ec66ac46d142c2ed2def3d71a86fadcbdfea3b4863f047f669a10be2384294786353d2ae4c5c6e'
